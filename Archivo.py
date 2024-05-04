b7a32b735c27c5c0ed1ab04292494dc024aa24d3
lcaminoa <161760246+lcaminoa@users.noreply.github.com>
2024-05-04 14:09:02 -0300

    Cuadrantes y varianzas

--- Archivo.py
+++ Archivo.py
@@ -1,5 +1,6 @@
 import numpy as np
 from PIL import Image
+from copy import deepcopy
 
 # Abrir imagen
 img = Image.open("baboon.jpg")
@@ -10,6 +11,35 @@ img_np = np.array(img)
 # Añadir padding de 2 pixeles con el color del pixel más cercano
 img_np_padding = np.pad(img_np, ((2, 2), (2, 2), (0, 0)), mode="edge")
 
-#Convertir la matriz NumPy a una imagen PIL y guardarla
-img_padding = Image.fromarray(img_np_padding)
-img_padding.save('imagen_padding.jpg')
+# Crear una copia profunda de la imagen array con el padding
+img_copy = deepcopy(img_np_padding)
+
+# Recorrer cada pixel de la imagen copiada
+for i in range(2, img_copy.shape[0]-2):
+    for j in range(2, img_copy.shape[1]-2):
+        
+        # Tomar un entorno de 5x5
+        entorno = img_copy[i-2:i+3, j-2:j+3]
+        
+        # Dividir este entorno en cuatro cuadrantes
+        cuadrantes = [entorno[:2, :2], entorno[:2, 2:], entorno[2:, :2], entorno[2:, 2:]]
+
+        # Puede que haya un error y no incluya la cruz. Sol: Que el slicing termine en 3.
+        
+        # Calcular la varianza de los canales rojo, verde y azul y sumar estas varianzas
+        varianzas = [np.var(cuadrante, axis=(0, 1)) for cuadrante in cuadrantes] # Se agrega a la lista las varianzas de cada cuadrante
+        """
+        axis=(0, 1) estaría calculando la varianza de cada canal de color en cada cuadrante.
+        """
+        total_variances = [np.sum(varianza) for varianza in varianzas]
+        
+        # Seleccionar el cuadrante con la suma de varianzas más baja
+        min_variance_quadrant = cuadrantes[np.argmin(total_variances)]
+        
+        # Calcular el promedio de cada color para los pixeles del cuadrante seleccionado
+        avg_color = np.mean(min_variance_quadrant, axis=(0, 1))
+        
+        # Asignar este promedio al pixel actual en la imagen copiada
+        img_copy[i, j] = avg_color
+
+# Devolver la imagen copiada modificada
