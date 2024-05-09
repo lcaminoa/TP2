1a3ac47ef643149e7f34fab4f6b2875e0cec43ee
Lautaro Caminoa <lcaminoa@udesa.edu.ar>
2024-05-09 17:16:19 -0300

    Terminado :)

--- cypher.py
+++ cypher.py
@@ -1,11 +1,15 @@
 from funciones import encriptador_mensaje, esconder_mensaje_imagen
 
-print("≡≡Encriptador≡≡")
-img_base = input("Ingrese nombre de la imagen a utilizar como base: ")
-mensaje = input("Ingrese el mensaje a esconder: ")
-salida = input("Ingrese nombre del archivo de salida: ")
+def main():
+    print("≡≡Encriptador≡≡")
+    img_base = input("Ingrese nombre de la imagen a utilizar como base: ")
+    mensaje = input("Ingrese el mensaje a esconder: ")
+    salida = input("Ingrese nombre del archivo de salida: ")
 
-mensaje_encriptado = encriptador_mensaje(mensaje)
-# Guardar la imagen con el mensaje escondido.
-imagen_encriptada = esconder_mensaje_imagen(img_base, mensaje_encriptado)
-imagen_encriptada.save(salida)
+    mensaje_encriptado = encriptador_mensaje(mensaje)
+    # Guardar la imagen con el mensaje escondido.
+    imagen_encriptada = esconder_mensaje_imagen(img_base, mensaje_encriptado)
+    imagen_encriptada.save(salida)
+
+while __name__ == "__main__":
+    main()
--- decypher.py
+++ decypher.py
@@ -1,7 +1,11 @@
 from funciones import escribir_mensaje, decifrar_mensaje_oculto
 
-print("≡≡Desencriptador≡≡")
-direc_imagen_encriptada = input("Ingrese nombre del archivo encriptado: ")
-print("")
-mensaje = escribir_mensaje(decifrar_mensaje_oculto(direc_imagen_encriptada))
-print(f"El mensaje oculto es: {mensaje}")
+def main():
+    print("≡≡Desencriptador≡≡")
+    direc_imagen_encriptada = input("Ingrese nombre del archivo encriptado: ")
+    print("")
+    mensaje = escribir_mensaje(decifrar_mensaje_oculto(direc_imagen_encriptada))
+    print(f"El mensaje oculto es: {mensaje}")
+
+while __name__ == "__main__":
+    main()
